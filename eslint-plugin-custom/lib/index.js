3503bfd6eac6aaab4530eb8255b8dafd12c5596f
Yoki Yu <yokiyuqian@gmail.com>
2021-10-20 10:17:40 +0800

    feat: update

--- eslint-plugin-custom/lib/index.js
+++ eslint-plugin-custom/lib/index.js
@@ -2,21 +2,39 @@
  * @fileoverview Custom eslint plugin practice
  * @author custom
  */
-"use strict";
+'use strict'
 
 //------------------------------------------------------------------------------
 // Requirements
 //------------------------------------------------------------------------------
 
-const requireIndex = require("requireindex");
+const requireIndex = require('requireindex')
 
 //------------------------------------------------------------------------------
 // Plugin Definition
 //------------------------------------------------------------------------------
 
-
 // import all rules in lib/rules
-module.exports.rules = requireIndex(__dirname + "/rules");
+// module.exports.rules = requireIndex(__dirname + '/rules')
 
-
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  rules: requireIndex(__dirname + '/rules'),
+  /* 
+    所有在config名下的配置这样使用：
+    在/some-project/.eslintrc.js中
+    module.exports = {
+      ...
+      extends: [plugin:custom/yourRonfigName]
+    }
+  */
+  configs: {
+    recommended: {
+      plugins: ['custom'],
+      rules: {
+        'custom/no-chinese-identifier': 2
+      }
+    }
+  }
+}
 
