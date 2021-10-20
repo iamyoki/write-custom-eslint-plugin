af19a5190d28a9bc3d147712b32f22f647c172c7
Yoki Yu <yokiyuqian@gmail.com>
2021-10-20 14:16:02 +0800

    feat: add prettier config in eslint options fields

--- eslint-config-custom/recommended.js
+++ eslint-config-custom/recommended.js
@@ -2,6 +2,13 @@ module.exports = {
   plugins: ['custom'],
   rules: {
     'custom/no-chinese-identifier': 2,
-    'no-alert': 2
+    'no-alert': 2,
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        semi: false
+      }
+    ]
   }
 }
--- example-usage/usage/use-prettier.js
+++ example-usage/usage/use-prettier.js
@@ -1,1 +1,5 @@
-const a = 'adad';
+const a = 'adad'
+const b = 'adsad'
+function helo(name) {
+  return 111
+}
